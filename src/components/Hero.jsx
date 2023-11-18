ed81fd262571042dc7bb9559c745cb9a242c70db
Nimesh9172 <nimeshdj123@gmail.com>
2023-11-18 20:22:26 +0530

    fixed bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <span className="hidden md:block">
+      <span className="hidden md:contents">
         <ComputersCanvas />
       </span>
 
